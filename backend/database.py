6a8afb565ab266b08af688cdfdcc7832d43ea67b
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-11-29 06:22:48 +0300

    refactor: change to MySQL db

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
 from sqlmodel import SQLModel, create_engine
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# sqlite_file_name = "database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
+# connect_args = {"check_same_thread": False}
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+# MySQL Connection
+mysql_url = "mysql+pymysql://portfolio_user:port123pass@localhost:3306/portfolio"
+
+engine = create_engine(mysql_url, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
