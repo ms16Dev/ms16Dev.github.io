5668112d91ffaf07f1f27d6969bfe5282cdd2048
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-12-17 02:53:23 +0300

    hotfix: correct cors origins

--- backend/main.py
+++ backend/main.py
@@ -25,8 +25,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://ms16dev.github.io"
-    "*"  # For development only
+    "https://ms16dev.github.io",
 ]
 
 app.add_middleware(
